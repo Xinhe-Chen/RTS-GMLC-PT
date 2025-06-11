03e08842be60041230f0ab3653503c024beb1b46
Xinhe Chen <xchen24@nd.edu>
2025-06-11 15:42:59 -0400

    fix startup bugs

--- general_gen_model.py
+++ general_gen_model.py
@@ -437,7 +437,7 @@ class PriceTakerRTSGMLC(ConcreteModel):
             self._get_valid_block_name(op_block_name) + "_startup_shutdown"
             )
             start_shut_blk = getattr(self, start_shut_blk_name)
-            self.period[t].hourly_startup_cost = start_shut_blk.startup_cost_at_t[t]
+            self.period[t].hourly_startup_cost = pyo.Expression(expr=start_shut_blk.startup_cost_at_t[t])
 
             # Set net cash inflow expression
             self.period[t].net_hourly_cash_inflow = Expression(
