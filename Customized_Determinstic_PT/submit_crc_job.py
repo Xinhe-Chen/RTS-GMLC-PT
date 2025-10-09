6d9cd80673ece3c5a9dbb84588cee2e9db181c5f
Xinhe Chen <xchen24@nd.edu>
2025-10-09 14:55:34 -0400

    update

--- Customized_Determinstic_PT/submit_crc_job.py
+++ Customized_Determinstic_PT/submit_crc_job.py
@@ -2,7 +2,7 @@ import os
 import json
 
 this_file_path = os.path.dirname(os.path.realpath(__file__))
-current_path = os.getcwd()
+# current_path = os.getcwd()
 
 def submit_job(gen_name, test_run=True):
 
@@ -11,11 +11,11 @@ def submit_job(gen_name, test_run=True):
     if not os.path.isdir(job_scripts_dir):
         os.mkdir(job_scripts_dir)
     if test_run:
-        test = "_test"
+        title = "Test"
     else:
-        test = ""
+        title = "Sweep"
 
-    file_name = os.path.join(job_scripts_dir, f"run_PT_opt_{gen_name}{test}"  + ".sh")
+    file_name = os.path.join(job_scripts_dir, f"{title}_run_PT_opt_{gen_name}"  + ".sh")
     # conda_env_path = os.path.join(current_path, "..", "..", ".conda", "envs", "idaes")
     conda_env_path = "idaes"
     with open(file_name, "w") as f:
@@ -24,7 +24,7 @@ def submit_job(gen_name, test_run=True):
             + "#$ -M xchen24@nd.edu\n"
             + "#$ -m ae\n"
             + "#$ -q long\n"
-            + "#$ -N " + f"run_PT_opt_{gen_name}{test}" + "\n"
+            + "#$ -N " + f"{title}_run_PT_opt_{gen_name}" + "\n"
             + f"conda activate {conda_env_path}\n"
             + "export LD_LIBRARY_PATH=~/.conda/envs/regen/lib:$LD_LIBRARY_PATH \n"
             + "module load gurobi\n"
@@ -37,9 +37,11 @@ def submit_job(gen_name, test_run=True):
 
 if __name__ == "__main__":
     
-    gen_path = os.path.join("..", "Data", "gen_dict.json")
-    with open(gen_path, 'rb') as f:
-        gen_dict = json.load(f)
-    gen_names = list(gen_dict["fossil"].keys())
-    for n in gen_names:
-        submit_job(n, test_run=False)
+    # gen_path = os.path.join("..", "Data", "gen_dict.json")
+    # with open(gen_path, 'rb') as f:
+    #     gen_dict = json.load(f)
+    # gen_names = list(gen_dict["fossil"].keys())
+    # for n in gen_names:
+    #     submit_job(n, test_run=False)
+
+    submit_job("101_STEAM_3", test_run=True)
